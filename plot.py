6e0a21db357aa53f6e4fb5525e7475d7eda22835
Erik Bernhardsson <erikbern@spotify.com>
2015-04-22 22:49:05 -0400

    more data

--- plot.py
+++ plot.py
@@ -14,10 +14,12 @@ for line in open('log.txt'):
         print 'could not parse', line
         continue
 
+print len(ts)
+
 c_norm = colors.Normalize(vmin=0.0, vmax=0.3)
 scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=plt.get_cmap('jet'))
 
 colors = [scalar_map.to_rgba(t) for t in ts]
 
-plt.scatter(xs, ys, c=colors)
+plt.scatter(xs, ys, c=colors, marker='x')
 plt.savefig('world.png')

74e0dda17b9c3231e7ac21634ef61561d6afaaea
Erik Bernhardsson <mail@erikbern.com>
2015-04-22 21:16:56 -0400

    initial commit

---
+++ plot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+
+xs, ys, ts = [], [], []
+for line in open('log.txt'):
+    try:
+        lon, lat, t = map(float, line.strip().split())
+        xs.append(lat)
+        ys.append(lon)
+        ts.append(t)
+    except:
+        print 'could not parse', line
+        continue
+
+c_norm = colors.Normalize(vmin=0.0, vmax=0.3)
+scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=plt.get_cmap('jet'))
+
+colors = [scalar_map.to_rgba(t) for t in ts]
+
+plt.scatter(xs, ys, c=colors)
+plt.show()
---
+++ query.py
@@ -0,0 +1,50 @@
+import random
+import ping
+import traceback
+import subprocess
+import time
+import os
+
+from multiprocessing.pool import ThreadPool, Pool
+
+FNULL = open(os.devnull, 'w')
+
+def get_random_ip():
+    return '.'.join(map(str, [random.randint(0, 255) for i in xrange(4)]))
+
+def ping_ip(ip, lat, lon):
+    try:
+        print ip, lat, lon, '?'
+        t0 = time.time()
+        status = subprocess.call(['ping', '-c1', '-t2', ip], stdout=FNULL, stderr=FNULL)
+        if status != 0:
+            return None
+        return ip, lat, lon, time.time() - t0
+    except:
+        traceback.print_exc()
+
+if __name__ == '__main__':
+    pool = ThreadPool(processes=100)
+    f = open('log.txt', 'a')
+
+    def cb(result):
+        print result
+        if result is None:
+            return
+        ip, lat, lon, t = result
+        print >>f, lat, lon, t
+
+    from geoip import geolite2
+
+    for i in xrange(10000):
+        ip = get_random_ip()
+        match = geolite2.lookup(ip)
+        if match is None or match.location is None:
+            continue
+        lat, lon = match.location
+        pool.apply_async(ping_ip, args=(ip, lat, lon), callback=cb)
+
+    pool.close()
+    pool.join()
+
+    f.close()
